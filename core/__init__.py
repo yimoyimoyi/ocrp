a37941917801ca1ca95dfef4f3537d5c0921419c
yimoyimoyi <3255961367@qq.com>
2026-05-24 21:15:45 +0800

    refactor: 调整导入顺序以提高代码可读性

--- core/__init__.py
+++ core/__init__.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 """core 包初始化。"""
 
+from core.ai_correction import AICorrector as AICorrector, load_correction_config as load_correction_config
+from core.frame_processor import (
+    FrameProcessor as FrameProcessor,
+    format_time as format_time,
+    get_similarity as get_similarity,
+)
 from core.ocr_engine import (
-    OCREngineManager,
-    BaseOCREngine,
-    PaddleOCREngine,
-    OpenAIVisionEngine,
-    OllamaVisionEngine,
-    LlamaCppEngine,
+    BaseOCREngine as BaseOCREngine,
+    LlamaCppEngine as LlamaCppEngine,
+    OCREngineManager as OCREngineManager,
+    OllamaVisionEngine as OllamaVisionEngine,
+    OpenAIVisionEngine as OpenAIVisionEngine,
+    PaddleOCREngine as PaddleOCREngine,
 )
-from core.ai_correction import AICorrector, load_correction_config
-from core.frame_processor import FrameProcessor, get_similarity, format_time
+from core.prompt_manager import PromptTemplateManager as PromptTemplateManager
 from core.result_processor import (
-    polish_results,
-    export_results,
-    GARBAGE_PATTERN,
+    GARBAGE_PATTERN as GARBAGE_PATTERN,
+    export_results as export_results,
+    polish_results as polish_results,
 )
-from core.prompt_manager import PromptTemplateManager
